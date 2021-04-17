5f24d616846efbe7c0c95c892fa70b790b71e194
Aslanyan94 <47784673+Aslanyan94@users.noreply.github.com>
2021-04-17 17:03:33 +0400

    conf

--- pysqlite.py
+++ pysqlite.py
@@ -41,3 +41,6 @@ locals()[args.command](args.db, args.table)
 # print(a)
 # # We can also close the connection if we are done with it.
 # # Just be sure any changes have been committed or they will be lost.
+
+# commit in main
+# must be conflict with second branch
